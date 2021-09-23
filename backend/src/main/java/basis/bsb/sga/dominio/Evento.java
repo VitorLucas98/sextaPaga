0253ddc4b148457c332ef987375808fc37db234a
VitorLucas98 <vitorluc.2013@gmail.com>
2021-09-23 20:56:55 -0300

    Adicionando funcionalidades da regra de negocio

--- backend/src/main/java/basis/bsb/sga/dominio/Evento.java
+++ backend/src/main/java/basis/bsb/sga/dominio/Evento.java
@@ -18,6 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Evento implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
