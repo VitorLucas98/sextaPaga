58b25b8f368654ecf4597c116a8c31c08d147140
VitorLucas98 <vitorluc.2013@gmail.com>
2021-09-25 19:06:24 -0300

    Todos testes concluidos

--- backend/src/test/java/basis/bsb/sga/web/rest/EventoRecursoTeste.java
+++ backend/src/test/java/basis/bsb/sga/web/rest/EventoRecursoTeste.java
@@ -5,7 +5,6 @@ import basis.bsb.sga.builder.EventoBuilder;
 import basis.bsb.sga.dominio.Evento;
 import basis.bsb.sga.servicos.dtos.EventoDTO;
 import basis.bsb.sga.util.TestUtil;
-
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,13 +16,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.nio.charset.StandardCharsets;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -34,9 +32,6 @@ public class EventoRecursoTeste {
 
     private static final String BASE_URL = "/api/eventos";
 
-    /*
-    private static final Long ID_INEXISTENTE = 10000l;
-    */
     public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(
             MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
@@ -88,16 +83,6 @@ public class EventoRecursoTeste {
         ).andExpect(status().isOk());
     }
 
-    /*
-    @Test
-    @SneakyThrows
-    public void buscarPorIdNaoEncontrado() {
-        String idEvento = "/" + ID_INEXISTENTE;
-        mockMvc.perform(get(BASE_URL + idEvento)
-                .contentType(APPLICATION_JSON_UTF8)
-        ).andExpect(status().isNotFound());
-    }
-    */
     @Test
     @SneakyThrows
     public void atualizacaoComSucesso() {
@@ -111,6 +96,24 @@ public class EventoRecursoTeste {
                 .contentType(APPLICATION_JSON_UTF8)
         ).andExpect(status().isOk());
 
+    }
+    @Test
+    @SneakyThrows
+    public void adiarEvento(){
+        Evento eventoSalvo = eventoBuilder.construir();
+        mockMvc.perform(put(BASE_URL+"/adiar/"+eventoSalvo.getId())
+                .contentType(APPLICATION_JSON_UTF8)
+        ).andExpect(status().isNoContent());
+    }
+    @Test
+    @SneakyThrows
+    public void trocarEvento(){
+        Evento evento1Salvo = eventoBuilder.construir();
+        Evento evento2Salvo = eventoBuilder.construir();
+        evento2Salvo.setDataEvento(evento1Salvo.getDataEvento().plusDays(7));
+        mockMvc.perform(put(BASE_URL+"/trocarevento/"+evento1Salvo.getId()+"/"+evento2Salvo.getId())
+                .contentType(APPLICATION_JSON_UTF8)
+        ).andExpect(status().isOk());
     }
 
 }
