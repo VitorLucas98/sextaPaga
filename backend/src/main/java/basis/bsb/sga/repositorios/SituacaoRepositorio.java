e771969358afc2c8ec808ac14a79f9a55681700a
VitorLucas98 <vitorluc.2013@gmail.com>
2021-09-28 21:54:08 -0300

    Adicionando listagem de situações

---
+++ backend/src/main/java/basis/bsb/sga/repositorios/SituacaoRepositorio.java
@@ -0,0 +1,9 @@
+package basis.bsb.sga.repositorios;
+
+import basis.bsb.sga.dominio.Situacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SituacaoRepositorio extends JpaRepository<Situacao, Long> {
+}
