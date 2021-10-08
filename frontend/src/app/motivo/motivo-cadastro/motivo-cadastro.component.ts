7a11d44b4ee96e7f24d5e5d77240baea6d65b227
DoglasBSB <doglas_bsb@hotmail.com>
2021-10-07 22:26:59 -0300

    crud motivo frontend

--- frontend/src/app/motivo/motivo-cadastro/motivo-cadastro.component.ts
+++ frontend/src/app/motivo/motivo-cadastro/motivo-cadastro.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MotivoService } from 'src/app/services/motivo.service';
@@ -6,6 +7,8 @@ import * as moment from 'moment';
 import { SelectItem } from 'src/app/models/SelectItem';
 import { MessageService } from 'primeng/api';
 import { MessagemUtils } from 'src/app/shared/mensagens-uteis';
+import { Motivo } from 'src/app/models/Motivo';
+import { CrudOperationEnum } from 'src/app/enums/Crud-Operation.enum';
 
 
 
@@ -16,62 +19,107 @@ import { MessagemUtils } from 'src/app/shared/mensagens-uteis';
 })
 export class MotivoCadastroComponent implements OnInit {
 
+
+
   public motivoForm: FormGroup;
   public fb: FormBuilder = new FormBuilder();
 
-  /*cargos: SelectItem[] = [];*/
+  
+  @Input() motivo: Motivo;
+  @Input() modoCrud: CrudOperationEnum = CrudOperationEnum.CREATE;
+  @Output() onCancel: EventEmitter<any> = new EventEmitter();
+  @Output() novaBusca: EventEmitter<any> = new EventEmitter();
+ 
+
 
   constructor(
     private service: MotivoService,
     private router: Router,
-    /*private cargoService: CargoService,*/
     private mensagem: MessageService) {
 
   }
 
   ngOnInit(): void {
     this.criaFormulario();
-    /*this.listaCargos();*/
-
+    this.preencherFormulario();
+    this.estadoFormulario(CrudOperationEnum.UPDATE == this.modoCrud || CrudOperationEnum.CREATE == this.modoCrud);
   }
-
-  /*public listaCargos(): void {
-    this.cargoService.buscarTodos().subscribe(res => {
-      this.cargos = [{
-        label: '--Selecione um cargo--',
-        value: null
-      } as SelectItem].concat(res);
-    })
-  }*/
 
   public criaFormulario(): void {
     this.motivoForm = this.fb.group({
       id: [null],
       titulo: ['', Validators.required],
-      descricao: ['', Validators.required],      
+      descricao: ['', Validators.required]      
     });
   }
 
+  preencherFormulario(): void {
+    this.motivoForm.get('id').setValue(this.motivo.id);
+    this.motivoForm.get('titulo').setValue(this.motivo.titulo);
+    this.motivoForm.get('descricao').setValue(this.motivo.descricao);
+  }
+
+  public estadoFormulario(habilitado: boolean): void {
+    habilitado ?  this.motivoForm.enable() : this.motivoForm.disable();
+  }
+
   public cancelar(): void {
+    this.onCancel.emit()
     this.router.navigateByUrl('motivos')
   }
 
-
-  public criar(): void {
-   
-    if(!this.motivoForm.valid) {
-      this.mensagem.add({ severity: 'error', summary: MessagemUtils.TITULO_DADOS_INVALIDOS, detail: MessagemUtils.MENSAGEM_ERRO_PREENCHIMENTO})
-      return;
+  public getTitulo(): string {
+    switch (this.modoCrud) {
+      case CrudOperationEnum.CREATE: return 'CADASTRO DE MOTIVO';
+      case CrudOperationEnum.UPDATE: return 'EDIÇÃO DE MOTIVO';
+      case CrudOperationEnum.READ: return 'VISUALIZAÇÃO DE MOTIVO';
     }
-    this.salvarMotivo();
   }
 
-  public salvarMotivo(): void {
+  public persistir(): Observable<Motivo> {
+    switch (this.modoCrud) {
+      case CrudOperationEnum.CREATE:
+        this.criarMotivo();
+        return
+      case CrudOperationEnum.UPDATE:
+        this.atualizarMotivo();
+        return
+      
+    }
+    throw Error('Não foi possível persistir os dados devido à falta de uma estratégia');
+  }
+
+  public criarMotivo(): void {
+    this.salvar();
     this.service.inserir(this.motivoForm.value).subscribe(() => {
       this.mensagem.add({ severity: 'success', summary: MessagemUtils.TITULO_SUCESSO, detail: MessagemUtils.MENSAGEM_DADOS_SALVOS });
       this.router.navigateByUrl('motivos')
+      this.onCancel.emit();
     })
   }
 
+  public atualizarMotivo(): void {
+    this.salvar();
+    this.service.atualizar(this.motivoForm.value, this.motivoForm.value.id).subscribe(() => {
+      this.mensagem.add({ severity: 'success', summary: MessagemUtils.TITULO_SUCESSO, detail: MessagemUtils.MENSAGEM_DADOS_SALVOS });
+      this.onCancel.emit();
+      this.novaBusca.emit();
+    })
+  }
+
+  public salvar(): void {
+        if (!this.motivoForm.valid) {
+      this.mensagem.add({ severity: 'error', summary: MessagemUtils.TITULO_DADOS_INVALIDOS, detail: MessagemUtils.MENSAGEM_ERRO_PREENCHIMENTO })
+      return;
+    }
+  }
+
+  public deletarMotivo(): void {
+    if (!this.motivoForm.valid) {
+      this.mensagem.add({ severity: 'success', summary: MessagemUtils.TITULO_DADOS_EXCLUIDOS, detail: MessagemUtils.MENSAGEM_DADOS_DELETADOS });
+      return;
+    };
+  
+  }
 
 }
--- frontend/src/app/shared/mensagens-uteis.ts
+++ frontend/src/app/shared/mensagens-uteis.ts
@@ -2,8 +2,13 @@ export class MessagemUtils{
     public static readonly TITULO_SERVICO_INDISPONIVEL: string = 'Serviço Temporariamente indisponivel!';
     public static readonly TITULO_DADOS_INVALIDOS : string = 'Preenchimento dos dados invalidados!';
     public static readonly TITULO_SUCESSO : string = 'Operação bem sucedida!';
+   
+    public static readonly TITULO_DADOS_EXCLUIDOS : string = 'Os Dados serão excluidos';
 
     public static readonly MENSAGEM_TENTE_NOVAMENTE: string = 'Tente novamente em alguns instantes...';
     public static readonly MENSAGEM_ERRO_PREENCHIMENTO: string = 'Erro no pŕeenchimento dos dados!';
     public static readonly MENSAGEM_DADOS_SALVOS : string = 'Dados foram salvos com sucesso!';
+
+    public static readonly MENSAGEM_DADOS_DELETADOS : string = 'Dados foram excluidos com sucesso!';
+
 }
